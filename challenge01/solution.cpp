615c456f53373d8a47b8582f32c5e777127ba752
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-08-29 13:35:37 +0000

    Challenge 01 submission

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,9 +1,60 @@
 // Challenge 01: Rotating Arrays
 
-// Main Execution
+// Michael McRoskey
+// Challenge 01
+// solution.cpp
 
-int main(int argc, char *argv[]) {
-    return 0;
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {	
+	
+	vector<int> array;					// create array as vector
+	int n, r, val;
+	char d;
+	
+	//------------Gather Input-------------//
+	
+	cin >> n;							// number of values
+	cin >> r;							// number of rotations
+	cin >> d;							// direction (R/L)
+	
+	// Store array
+	for (int i=0; i<n; i++){
+		cin >> val;
+		array.push_back(val);
+	}
+	
+	//------------Perform Shift------------//
+	
+	if (d=='R' || d=='r') {				// right shift
+	
+		for (int i=0; i<r; i++) {		// run for r rotations
+			for (int j=n; j>0; j--)		// start with last element
+				array[j] = array[j-1];	
+			array[0] = array[n];		// replace first element
+		}
+		
+	} else if (d=='L' || d=='l') {		// left shift
+	
+		for (int i=0; i<r; i++) {
+			array[n] = array[0];		// replace last element
+			for (int j=0; j<n; j++)		// start with first element
+				array[j] = array[j+1];
+		}
+		
+	} else {
+		cout << "Error: not a valid direction." << endl;
+	}
+	
+	//-----------Display Vector-----------//
+	
+	for (int i=0; i<n; i++)
+		cout << array[i] << ' ';
+
+	return 0;
 }
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
