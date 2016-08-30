b0a816deb537d3977b0afeee09ac0c59945bbee8
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-08-30 11:34:32 +0000

    Added loop for several entries

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -15,46 +15,51 @@ int main() {
 	int n, r, val;
 	char d;
 	
-	//------------Gather Input-------------//
+	for (k=0; k<100; k++) {
 	
-	cin >> n;							// number of values
-	cin >> r;							// number of rotations
-	cin >> d;							// direction (R/L)
-	
-	// Store array
-	for (int i=0; i<n; i++){
-		cin >> val;
-		array.push_back(val);
-	}
-	
-	//------------Perform Shift------------//
-	
-	if (d=='R' || d=='r') {				// right shift
-	
-		for (int i=0; i<r; i++) {		// run for r rotations
-			for (int j=n; j>0; j--)		// start with last element
-				array[j] = array[j-1];	
-			array[0] = array[n];		// replace first element
+		//------------Gather Input-------------//
+		
+		cin >> n;							// number of values
+		cin >> r;							// number of rotations
+		cin >> d;							// direction (R/L)
+		
+		// Store array
+		for (int i=0; i<n; i++){
+			cin >> val;
+			array.push_back(val);
 		}
 		
-	} else if (d=='L' || d=='l') {		// left shift
-	
-		for (int i=0; i<r; i++) {
-			array[n] = array[0];		// replace last element
-			for (int j=0; j<n; j++)		// start with first element
-				array[j] = array[j+1];
+		//------------Perform Shift------------//
+		
+		if (d=='R' || d=='r') {				// right shift
+		
+			for (int i=0; i<r; i++) {		// run for r rotations
+				for (int j=n; j>0; j--)		// start with last element
+					array[j] = array[j-1];	
+				array[0] = array[n];		// replace first element
+			}
+			
+		} else if (d=='L' || d=='l') {		// left shift
+		
+			for (int i=0; i<r; i++) {
+				array[n] = array[0];		// replace last element
+				for (int j=0; j<n; j++)		// start with first element
+					array[j] = array[j+1];
+			}
+			
+		} else {
+			cout << "Error: not a valid direction." << endl;
 		}
 		
-	} else {
-		cout << "Error: not a valid direction." << endl;
+		//-----------Display Vector-----------//
+		
+		for (int i=0; i<n; i++)
+			cout << array[i] << ' ';
+		
 	}
-	
-	//-----------Display Vector-----------//
-	
-	for (int i=0; i<n; i++)
-		cout << array[i] << ' ';
 
 	return 0;
 }
+
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
