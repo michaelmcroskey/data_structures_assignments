74f1a7b7f3a1bf4cb491e391594dfdf2144e55d0
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-09 18:53:17 +0100

    Omg I've never submitted an assignment this early

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -23,12 +23,14 @@ void write_vector(const vector<T>& V){
 
 int main()
 {
+	vector<int> values, sign;
 	bool firstline = true;
 	string line;
 	int N;
-	vector<int> integers;
 	
-	while (getline(std::cin, line)){		// read input
+	//-------------Gather/Parse Input-------------//
+	//      push_back values into int vector      //	
+	while (getline(std::cin, line)){
 		if (line.empty()) break;
 		
 		if (firstline) {
@@ -37,26 +39,60 @@ int main()
 			continue;
 		}
 		
-		//string input = "-20 -3916237 -357920 -3620601 7374819 -7330761 30 6246457 -6461594 266854";
 		istringstream iss(line);
 		copy(istream_iterator<int>(iss),
 		     istream_iterator<int>(),
-		     back_inserter(integers));
+		     back_inserter(values));
 	}
 	
-	for (size_t i=0; i != integers.size(); ++i)
-		integers[i] = abs(integers[i]);
+	vector<int> signs = values;
 	
-	sort(integers.begin(), integers.end());
+	//-------------Sort Vector/Array--------------//
+	//   first convert to abs-val and then sort   //
 	
+	for (size_t i=0; i != values.size(); ++i){
+		if (values[i] < 0){
+			sign.push_back(-1);
+		} else {
+			sign.push_back(1);
+		}
+		values[i] = abs(values[i]);
+	}
 	
+	sort(values.begin(), values.end());
 	
-	cout << "After" << endl;
-	write_vector(integers); 	
+	//------------Find Closest Numbers------------//
+	// compute each difference and store indexes  //	
+	int min = values[values.size()];
+	int diff, index;
+	
+	for (size_t i=0; i != values.size()-1; ++i){
+		diff = values[i+1] - values[i];
+		if (diff<min){
+			index = i;
+		}
+	}
+	
+	//-------------Account for Signs--------------//
+	//   check each element for the proper sign   //
+	
+	int close1 = values[index];
+	int close2 = values[index+1];	
+	
+	for (size_t i=0; i != values.size()-1; ++i){
+		if (signs[i] == close1 || abs(signs[i]) == close1){
+			close1 = signs[i];
+		}
+		if (signs[i] == close2 || abs(signs[i]) == close2){
+			close2 = signs[i];
+		}
+	}
+	
+	//--------------Display Results---------------//
+	//   display results close #1 and close #2    //	
+	cout << close1 << " " << close2 << endl;
     
     return EXIT_SUCCESS;
 }
-
-//------------Main Execution-------------//
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
