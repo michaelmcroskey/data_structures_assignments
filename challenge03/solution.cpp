7f103cca221a0ebf6d10b2e5ae1d977f609ccb26
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-09 01:46:54 +0100

    Inputting correctly, but time to call it a night

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -1,17 +1,54 @@
 // Challenge 03: Closest Numbers
 
+// Michael McRoskey
+// Challenge 02
+// solution.cpp
+
 #include <algorithm>
 #include <climits>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-// Main Execution
+template <typename T>
+void write_vector(const vector<T>& V)
+{
+    for(int i=0; i < V.size(); i++)
+        cout << V[i] << " ";
+}
 
-int main(int argc, char *argv[]) {
+int main()
+{
+	bool firstline = true;
+	string line;
+	int N;
+	
+	while (getline(std::cin, line)){		// read input
+		if (line.empty()) break;
+		
+		if (firstline) {
+			N = stoi(line);
+			firstline = false;
+			continue;
+		}
+		
+		//string input = "-20 -3916237 -357920 -3620601 7374819 -7330761 30 6246457 -6461594 266854";
+		istringstream iss(line);
+		vector<string> tokens{istream_iterator<string>{iss},
+	                      istream_iterator<string>{}};
+	
+	write_vector(tokens); // fix this scope issue
+	}
+	
+	
+    
     return EXIT_SUCCESS;
 }
+
+//------------Main Execution-------------//
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
