eb80012bdba068e4262cbc762ac78f3c2d965839
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-09 17:57:16 +0100

    Sorting and absolute value working

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -15,10 +15,10 @@
 using namespace std;
 
 template <typename T>
-void write_vector(const vector<T>& V)
-{
+void write_vector(const vector<T>& V){
     for(int i=0; i < V.size(); i++)
         cout << V[i] << " ";
+	cout << endl;
 }
 
 int main()
@@ -26,6 +26,7 @@ int main()
 	bool firstline = true;
 	string line;
 	int N;
+	vector<int> integers;
 	
 	while (getline(std::cin, line)){		// read input
 		if (line.empty()) break;
@@ -38,13 +39,20 @@ int main()
 		
 		//string input = "-20 -3916237 -357920 -3620601 7374819 -7330761 30 6246457 -6461594 266854";
 		istringstream iss(line);
-		vector<string> tokens{istream_iterator<string>{iss},
-	                      istream_iterator<string>{}};
-	
-	write_vector(tokens); // fix this scope issue
+		copy(istream_iterator<int>(iss),
+		     istream_iterator<int>(),
+		     back_inserter(integers));
 	}
 	
+	for (size_t i=0; i != integers.size(); ++i)
+		integers[i] = abs(integers[i]);
 	
+	sort(integers.begin(), integers.end());
+	
+	
+	
+	cout << "After" << endl;
+	write_vector(integers); 	
     
     return EXIT_SUCCESS;
 }
