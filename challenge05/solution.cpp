b4b4a6cb65c410501a75dcda7f6be922462c498f
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-20 19:42:19 +0100

    Added comments and fixed extra line

--- challenge05/solution.cpp
+++ challenge05/solution.cpp
@@ -2,10 +2,6 @@
 
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <iomanip>
-#include <algorithm>
-#include <memory>
 
 using namespace std;
 
@@ -15,35 +11,38 @@ int main() {
    
 	int N = 0, tree=1;
 	bool bst = false;
+	string line;
 	
-	while (1) {
-		
-		int c = std::cin.peek();  // peek character
-		if (c == EOF) break;
-
-		cin >> N;
+	while (cin >> N) {
+		// initialize array
 		int a[N];
 		
+		// load values into array
 		for (int i=0; i<N; i++) {
 			cin >> a[i];
 		}
 		
 		for (int i=0; i<N/2; i++) {
+			// if blank node, ignore it
 			if (a[i] == -1) continue;
 			if (a[2*i+1] == -1) continue;
 			if (a[2*i+2] == -1) continue;
 			
+			// check left child
 			if (a[2*i+1]>a[i]){
 				bst = false;
 				break;
+			// check right child
 			} else if (a[2*i+2]<a[i]){
 				bst = false;
 				break;
+			// if everything passes, good to go
 			} else {
 				bst = true;
 			}
 		}
 		
+		// output result
 		if (bst){
 			cout << "Tree " << tree << " is a BST" << endl;
 		} else {
