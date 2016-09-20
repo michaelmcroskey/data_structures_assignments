6323349c5a85b071fd4be90e6063528e2a45c866
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-20 16:26:18 +0100

    Think it's working

--- challenge04/solution.cpp
+++ challenge04/solution.cpp
@@ -5,18 +5,20 @@
 #include <fstream>
 #include <iomanip>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 // Main Execution
+struct Card {
+    int rank, suit;
+	string name;
+};
 
-template <typename T>
-void display(vector<T> &v){
-	for(int i = 0; i < v.size(); i++){
-		cout << v[i] << " ";
-	}
-    cout << "\n" << endl;
-}
+void royalty(Card *c, char rank);
+void convert_suits(Card *c, char suit);
+bool compare_rank(const Card &a, const Card &b);
+bool compare_suit(const Card &a, const Card &b);
 
 int main(int argc, char *argv[]) {
     
@@ -24,39 +26,32 @@ int main(int argc, char *argv[]) {
     //      push_back values into int vector      //
 
 	int N;
-	char temp;
+	char rank, suit;
 	cin >> N;
-	
-	struct Card {
-	    int rank;
-		string name;
-	    char suit;
-		int index;
-	};
 
 	vector<Card> cards;
+	cout << endl;
 	
 	for (int i=0; i<N; i++) {
 		cards.push_back(Card());
+		cin >> cards[i].name >> rank >> suit;
 		
-		cin >> cards[i].name >> temp >> cards[i].suit;
+		royalty(&cards[i], rank);
+		convert_suits(&cards[i], suit);
 		
-		if (temp == 'J'){
-			cards[i].rank = 11;
-		} else if (temp == 'Q') {
-			cards[i].rank = 12;
-		} else if (temp == 'K') {
-			cards[i].rank = 13;
-		} else if (temp == 'A') {
-			cards[i].rank = 14;
-		} else {
-			cards[i].rank = (int)temp;
-		}
-		
-		cards[i].index = i;
+//		cout << cards[i].name << " " << cards[i].rank << " " << cards[i].suit << endl;
 	}
 	
-	cout << cards[1].name << cards[1].rank << cards[1].suit << endl;
+	std::sort(cards.begin(), cards.end(), compare_suit);
+	std::sort(cards.begin(), cards.end(), compare_rank);
+	
+	for (int i=0; i<N; i++) {
+		cout << cards[i].name << " ";
+		if (i==N-1) {
+			break;
+		}
+	}
+	
 
 //    while (getline(std::cin, line)){
 //        if (line.empty()) continue;
@@ -72,6 +67,40 @@ int main(int argc, char *argv[]) {
 //	}
 
     return EXIT_SUCCESS;
+}
+
+void royalty(Card *c, char rank){
+	if (rank == 'J'){
+		c->rank = 11;
+	} else if (rank == 'Q') {
+		c->rank = 12;
+	} else if (rank == 'K') {
+		c->rank = 13;
+	} else if (rank == 'A') {
+		c->rank = 14;
+	} else {
+		c->rank = rank - '0';
+	}
+}
+
+void convert_suits(Card *c, char suit){
+	if (suit == 'C'){
+		c->suit = 1;
+	} else if (suit == 'D') {
+		c->suit = 2;
+	} else if (suit == 'H') {
+		c->suit = 3;
+	} else if (suit == 'S') {
+		c->suit = 4;
+	}
+}
+
+bool compare_rank(const Card &a, const Card &b){
+    return a.rank > b.rank;
+}
+
+bool compare_suit(const Card &a, const Card &b){
+    return a.suit > b.suit;
 }
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
