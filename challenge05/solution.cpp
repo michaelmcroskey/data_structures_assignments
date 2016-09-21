84ac9ce37e8426d1646d890264f7cd9a35cc0c63
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-21 23:00:43 +0100

    Fixing out of bounds memory again

--- challenge05/solution.cpp
+++ challenge05/solution.cpp
@@ -26,7 +26,7 @@ int main() {
 			// if blank node, ignore it
 			if (a[i] == -1) continue;
 			if (a[2*i+1] == -1) continue;
-			if ((i!=N/2) && a[2*i+2] == -1) continue;
+			if ((i!=N/2) && (a[2*i+2] == -1)) continue;
 			
 			// check left child
 			if (a[2*i+1]>a[i]){
