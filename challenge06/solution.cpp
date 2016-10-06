6d2a96d1111b17a763c75b2daf2adc209153e104
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-10-06 21:03:10 +0100

    Think I fixed it

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -11,14 +11,9 @@ struct Node{
     struct Node *right;
 };
 
-Node* FindMin(Node* root){
-    while(root->left != NULL) root = root->left;
-    return root;
-}
-
 Node* FindLCA(Node* root, int a, int b){
     int min, max, curr;
-    bool again = true;
+
     if (a==b) return root;
     if (a>b){
         max = a;
@@ -28,55 +23,18 @@ Node* FindLCA(Node* root, int a, int b){
         min = a;
     }
     
-    while (again){
+    while (1){
         curr = root->data;
         if ((curr > min) && (curr > max)){
             root = root->left;
-            again = true;
         } else if ((curr < min) && (curr < max)){
             root = root->right;
-            again = true;
         } else {
             return root;
-            //if ((curr < min) && (curr < max))
         }
     }
     
     return root;
-}
-
-struct Node* Delete(struct Node *root, int data){
-    if(root == NULL) return root;
-    else if(data < root->data) root->left = Delete(root->left,data);
-    else if(data > root->data) root->right = Delete(root->right, data);
-    else {
-        // Case 1: No Child
-        if(root->left == NULL && root->right == NULL){
-            delete root;
-            root = NULL;
-        // Case 2: one child
-        } else if(root->left == NULL){
-            struct Node *temp = root;
-            root = root->right;
-            delete temp;
-        } else if(root->right == NULL){
-            struct Node *temp = root;
-            root = root->left;
-            delete temp;
-        } else{
-            struct Node *temp = FindMin(root->right);
-            root->data = temp->data;
-            root->right = Delete(root->right, temp->data);
-        }
-    }
-    return root;
-}
-
-void Inorder(Node *root){
-    if(root == NULL) return;
-    Inorder(root->left);
-    cout << " "<< root->data;
-    Inorder(root->right);
 }
 
 Node* Insert(Node *root, char data){
@@ -92,13 +50,24 @@ Node* Insert(Node *root, char data){
     return root;
 }
 
+void deleteTree(Node* root){
+    if(root==NULL) 
+        return;
+    if(root->left != NULL)
+        deleteTree(root->left);
+    if(root->right != NULL)
+        deleteTree(root->right);
+    delete t;
+    return;
+}
+
 int main (){
     
     int N, P, value, a, b;
     vector<int> pairs;
-    Node* root = NULL;
     
     while (cin >> N){
+        Node* root = NULL;
         // Exit at sentinel value
         if (N==-1) break;
         
@@ -116,14 +85,20 @@ int main (){
             pairs.push_back(a);
             pairs.push_back(b);
         }
-    }
-    
-    for (int i=0; i<(2*P); i+=2){
-        a = pairs.at(i);
-        b = pairs.at(i+1);
         
-        cout << FindLCA(root, a, b)->data << endl;
-    }
+        for (int i=0; i<(2*P); i+=2){
+            a = pairs.at(i);
+            b = pairs.at(i+1);
+            
+            cout << FindLCA(root, a, b)->data << endl;
+        }
     
+    cout << endl;
+    
+    deleteTree(root);
+    root = NULL;
+    pairs.clear();
+    }
+
 }
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
