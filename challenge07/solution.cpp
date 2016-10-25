c9cc9f45929a5772c4b913034040d7c966eb387e
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-10-25 18:50:27 +0100

    Finished challenge

--- challenge07/solution.cpp
+++ challenge07/solution.cpp
@@ -1,12 +1,131 @@
 // Challenge 07: Linked List Deduplication
+// Michael McRoskey
+
+#include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 // Structures
 
+class MyList{
+    
+    struct Node{
+        int data;
+        Node *next;
+    };
+    
+    public:
+        MyList(){
+            head = nullptr;
+        }
+        
+        void insert(int value){
+            
+            if (head == nullptr){
+                Node *first = new Node();
+                first->data = value;
+                first->next = nullptr;
+                head = first;
+            } else {
+                Node *newNode = new Node();
+                newNode->data = value;
+                newNode->next = nullptr;
+
+                Node *curr = head;
+                while(curr) {
+                    if(curr->next == nullptr) {
+                        curr->next = newNode;
+                        return;
+                    }
+                    curr = curr->next;
+                }
+            }  
+        }
+        
+        void remove(Node *old){
+            
+            Node *curr = head;
+            
+            // If first node
+            // if (old == head){} // irrelevant
+            
+            // If last node
+            if (old->next == nullptr){
+                while (curr){
+                    if (curr->next == old){
+                        curr->next = nullptr;
+                        delete old;
+                        break;
+                    }
+                    curr = curr->next;
+                }
+            } else {
+                Node *next = old->next;
+                while (curr){
+                    if (curr->next == old){
+                        curr->next = next;
+                        delete old;
+                        break;
+                    }
+                    curr = curr->next;
+                }
+            }
+        }
+        
+        void deduplicate(){
+            unordered_set<int> s;
+            Node *curr = head;
+
+                while(curr) {
+                    bool not_in_set = (s.find(curr->data) == s.end());
+                    if (not_in_set){     // not found, so insert to set
+                        s.insert(curr->data);                        
+                    } else {                // found, so remove from list
+                        remove(curr);
+                    }
+                    curr = curr->next;      // increment curr to next node
+                }
+        }
+        
+        void display(){
+            Node *curr = head;
+            while(curr != nullptr){
+                cout << curr->data << " ";
+                curr = curr->next;
+            }
+            cout << endl;
+        }
+        
+        void clear(){
+            Node *tmpNode;
+            while(head) {
+                tmpNode = head;
+                head = tmpNode->next;
+                delete tmpNode;
+            }
+        }
+
+    private:
+        Node *head;
+};
+
 // Main Execution
 
 int main(int argc, char *argv[]) {
+    
+    int N, x;
+    MyList l;
+    
+    while(cin >> N){
+        for (int i=0; i<N; i++){
+            cin >> x;
+            l.insert(x);
+        }
+        l.deduplicate();
+        l.display();
+        l.clear();
+    }    
     return EXIT_SUCCESS;
 }
 
