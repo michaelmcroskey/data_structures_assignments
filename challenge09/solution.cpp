847c19d0ccbca36a28301a24e01f7018353d43ba
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-11-03 18:12:34 +0000

    Working

--- challenge09/solution.cpp
+++ challenge09/solution.cpp
@@ -7,9 +7,33 @@
 #include <unordered_set>
 #include <typeinfo>
 
+#define UMAP unordered_map<char, unordered_set<char>>
+
 using namespace std;
 
-bool valid_path(unordered_map<char, unordered_set<char>> map, char n1, char n2){
+void print_map(UMAP map){
+    for (auto node : map){
+        cout << node.first << " :: ";
+        for (auto connected_node : node.second){
+            cout << connected_node << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+// Inserts every node in path
+UMAP expand_nodes(UMAP map){
+    for (auto& node : map)
+        for (auto connected_node : node.second)
+            for (auto i : map[connected_node])
+                if (i != node.first)
+                    node.second.insert(i);
+    return map;
+}
+
+bool valid_path(UMAP map, char n1, char n2){
+    // Return if n2 is in n1's connected nodes
     return map[n1].count(n2) > 0;
 }
 
@@ -18,47 +42,39 @@ bool valid_path(unordered_map<char, unordered_set<char>> map, char n1, char n2){
 int main(int argc, char *argv[]) {
     
     int NEDGES, NPATHS, graph = 1;
-    bool path;
+    bool path, first_pass = true;
     
     // Each Graph --------------------------------
     while (cin >> NEDGES){
         
+        if (first_pass){
+            first_pass = false;
+        } else {
+            cout << endl;
+        }
+        
         // Build Graph ---------------------------
-        unordered_map<char, unordered_set<char>> map;
+        UMAP map;
         char A, B;
         
         for (int i=0; i<NEDGES; i++){
             cin >> A >> B;
             // Element not in graph, insert
             if (map.find(A) == map.end()){
-                unordered_set<char> set;
-                set.insert(B);
+                unordered_set<char> set = {B};
                 map.insert({A, set});
                 
             // Element in graph, update connected nodes
             } else {
                 map.find(A)->second.insert(B);
             }
-            
-            // Recursively add connected nodes
-            for (auto node : map){
-                for (auto connected_node : node.second){
-                    
-                    // Should be a set?
-                    auto myset = map.find(connected_node)->second;
-                    
-                    for (auto i : myset){
-                        node.second.insert(i);
-                    }
-                    // For each node, add connected nodes
-                    //node.second.insert(myset.begin(), myset.end());
-
-                    //cout << typeid(map.find(connected_node)->second).name() << endl;
-                    //(map.find(node.first)->second).insert(map.find(connected_node)->second);
-                }
-            }
         }
         
+        // Add connected nodes -------------------
+        for (int i=0; i<map.size(); i++){
+            map = expand_nodes(map);
+        }
+
         // Store Paths ---------------------------
         cin >> NPATHS;
 
@@ -77,10 +93,8 @@ int main(int argc, char *argv[]) {
                 << not_path << " path from " << n1
                 << " to " << n2 << endl;
         }
-        
         graph++;
     }
-    
     return EXIT_SUCCESS;
 }
 
